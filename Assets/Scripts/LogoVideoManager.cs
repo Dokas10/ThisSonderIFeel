a37f0845ee9ccbba652bc7e96253923350504fd4
Dokas10 <andremar1000@gmail.com>
2020-12-12 18:54:40 +0000

    Added programming to animation

---
+++ Assets/Scripts/LogoVideoManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class LogoVideoManager : MonoBehaviour
+{
+
+    public VideoPlayer video;
+    public double time;
+    public double currentTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = video.clip.length;
+        video.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime = video.time;
+
+        if(currentTime >= time)
+            //Todo: Change Scene
+    }
+}
